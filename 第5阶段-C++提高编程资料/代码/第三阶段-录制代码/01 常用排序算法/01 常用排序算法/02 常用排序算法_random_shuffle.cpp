1626b32c800a88f9c687d11f4ab5646e1242c3e6
hujingcaho <1430978392@qq.com>
2025-08-06 20:51:52 +0800

    style:恢复代码格式
    - 移除所有代码注释
    - 恢复代码缩进
    - 更新部分代码实现，如使用更现代的随机数生成方式

--- 第5阶段-C++提高编程资料/代码/第三阶段-录制代码/01 常用排序算法/01 常用排序算法/02 常用排序算法_random_shuffle.cpp
+++ 第5阶段-C++提高编程资料/代码/第三阶段-录制代码/01 常用排序算法/01 常用排序算法/02 常用排序算法_random_shuffle.cpp
@@ -1,38 +1,41 @@
-//#include<iostream>
-//using namespace std;
-//#include <vector>
-//#include <algorithm>
-//#include <ctime>
-//
-////常用排序算法  random_shuffle
-//
-//void myPrint(int val)
-//{
-//	cout << val << " ";
-//}
-//
-//void test01()
-//{
-//	srand((unsigned int)time(NULL));
-//
-//	vector<int>v;
-//	for (int i = 0; i < 10; i++)
-//	{
-//		v.push_back(i);
-//	}
-//
-//	//利用洗牌 算法 打乱顺序
+#include<iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <random>
+
+//常用排序算法  random_shuffle
+
+void myPrint(int val)
+{
+	cout << val << " ";
+}
+
+void test01()
+{
+	srand((unsigned int)time(NULL));
+
+	vector<int>v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+    random_device rd;                       // 真随机数种子（非确定性）
+    default_random_engine rng(rd());        // 随机数引擎
+	//利用洗牌 算法 打乱顺序
 //	random_shuffle(v.begin(), v.end());
-//
-//	for_each(v.begin(), v.end(), myPrint);
-//	cout << endl;
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+	shuffle(v.begin(), v.end(),rd);
+
+	for_each(v.begin(), v.end(), myPrint);
+	cout << endl;
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
